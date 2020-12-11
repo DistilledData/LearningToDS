e0d174510d8550ba0cac87ab04660179312faa43
Brian Kelly <bak438@nyu.edu>
2020-12-11 16:29:30 -0500

    update _toc and build_notebook to use no subdirectories

--- build_notebook.sh
+++ build_notebook.sh
@@ -9,33 +9,25 @@ do
     cp $file ${book_dir}/$file
 done
 
-#copy images
-image_dir=${book_dir}/images
-source_dir=images
-rm ${image_dir} || true
-cp -r ${source_dir} ${image_dir}
-
 #copy notebook files
-notebook_dir=${book_dir}/notebooks
-mkdir ${notebook_dir} || true
 source_dir=notebooks
-for file in Probability_Basics.ipynb Probability_Basics_II.ipynb Probability_Basics_III.ipynb Common_Probability_Distributions_Discrete.ipynb Classification_Basics.ipynb
+notebook_names=(Probability_Basics Probability_Basics_II Probability_Basics_III Common_Probability_Distributions_Discrete Classification_Basics)
+for file in ${notebook_names[@]}
 do
-    rm ${notebook_dir}/$file || true
-    cp ${source_dir}/$file ${notebook_dir}
+    cp ${source_dir}/${file}.ipynb ${book_dir}
 done
 
 jupyter-book build ${book_dir}
 
-pushd ${book_dir}/_build/html/notebooks
-for file in $(ls)
+pushd ${book_dir}/_build/html
+for file in ${notebook_names[@]}
 do
     #need to change master to main in binder url for binder to work
-    sed -i 's/\/master?/\/main?/' $file
+    sed -i 's/\/master?/\/main?/' $file.html
     #insert MathJax script that renders latex correctly in html
-    sed -i '/<head>/a <script src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" type="text/javascript"></script>' Probability_Basics.html
+    sed -i '/<head>/a <script src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" type="text/javascript"></script>' $file.html
 done
 popd
 
 #publish code to github pages
-ghp-import -n -p -f ${book_dir}/_build/html
+#ghp-import -n -p -f ${book_dir}/_build/html
